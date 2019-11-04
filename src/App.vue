bf033ba87b748fcc48ee8c784aad98473716be45
Kevin McGuire <kevin@givegab.com>
2019-11-04 15:46:41 -0500

    add cancel button

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,9 @@
             label.m-3
               p.text-gray-700.mb-2 Gallons
               input.fillup-gallons(type="text" v-model="fillup_gallons" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline")
-          button.fillup-submit.block.mt-5(type="button" :disabled="!isValidNew()" @click="addFillup()" :class="{'opacity-50 pointer-events-none': !isValidNew(), 'hover:bg-orange-700': isValidNew() }" class="inline-block bg-orange-500 text-white font-bold py-2 px-4 rounded-full") Save
+          div
+            button.fillup-submit.block.mt-5(type="button" :disabled="!isValidNew()" @click="addFillup()" :class="{'opacity-50 pointer-events-none': !isValidNew(), 'hover:bg-orange-700': isValidNew() }" class="inline-block bg-orange-500 text-white font-bold py-2 px-4 rounded-full") Save
+            button(@click="show_new_fillup = !show_new_fillup" class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full ml-2") Cancel
           
         div(v-for="fillup in recentFillups").border-t-2.border-gray-500.border-solid
           form.border-solid.border-red-500.border-2.px-4.py-4.rounded.my-5(v-if="fillup.editing" v-on:submit.prevent="doFillupEdit(fillup)")
