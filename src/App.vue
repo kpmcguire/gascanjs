2502a359d99a4be7a5ef3e24bb348922dbb5b0e6
Kevin McGuire <kevin@givegab.com>
2019-11-04 15:20:50 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,119 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div.flex.flex-col.h-full
+    .header.bg-bg
+      .max-w-6xl.mx-auto
+        h1 
+          img.logo(src="/images/gascan.svg" alt="Gascan - Gas Mileage Tracker")
+    .main.max-w-6xl.mx-auto.flex-1.p-5.w-full
+      h2.font-bold.text-2xl.mb-3 Your Cars
+      ul.mb-3.-m-2.flex.content-between
+        li.m-2(v-for="(mpg, car) in total_avgMPG") 
+          div.text-gray-600.mb-1 {{car}} 
+          div.text-white.bg-bg.px-3.py-1.rounded.leading-tight
+            div.text-sm MPG
+            div.text-2xl.font-bold {{mpg}}
+
+      section.fillupsapp(v-cloak)
+        a(href="#" v-if="!show_new_fillup" @click="show_new_fillup = !show_new_fillup" class="inline-block mb-5 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full") Add new fillup
+        form.border-solid.border-red-500.border-2.px-4.py-4.rounded(v-on:submit.prevent v-if="show_new_fillup")
+          .flex.flex-wrap.-m-3
+            label.m-3
+              p.text-gray-700.mb-2 Car
+              select(v-model="fillup_car" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-40")
+                option(value='' disabled) -- Select a Car --
+                option(v-if="new_car" :value="fillup_car") {{fillup_car}}
+                option(v-for="car in uniqueCars" :value="car.car") {{car.car}}
+              button.new-car.block.mt-1(@click="addNewCar" class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-1 px-2 rounded-full text-xs mt-2") add car
+
+            label.m-3
+              p.text-gray-700.mb-2 Date
+              flat-pickr(v-model="fillup_date" :config="flatpickr_config" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-56")
+            label.m-3
+              p.text-gray-700.mb-2 Starting Odometer
+              input.fillup-starting-odo(type="text" v-model="fillup_starting_odo" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline")
+            label.m-3
+              p.text-gray-700.mb-2 Ending Odometer
+              input.fillup-ending-odo(type="text" v-model="fillup_ending_odo" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline") 
+            label.m-3
+              p.text-gray-700.mb-2 Miles      
+              div.leading-tight.py-2(v-if="fillup_ending_odo && fillup_starting_odo") {{fillup_ending_odo - fillup_starting_odo}}
+            label.m-3
+              p.text-gray-700.mb-2 Gallons
+              input.fillup-gallons(type="text" v-model="fillup_gallons" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline")
+          button.fillup-submit.block.mt-5(type="button" :disabled="!isValidNew()" @click="addFillup()" :class="{'opacity-50 pointer-events-none': !isValidNew(), 'hover:bg-orange-700': isValidNew() }" class="inline-block bg-orange-500 text-white font-bold py-2 px-4 rounded-full") Save
+          
+        div(v-for="fillup in recentFillups").border-t-2.border-gray-500.border-solid
+          form.border-solid.border-red-500.border-2.px-4.py-4.rounded.my-5(v-if="fillup.editing" v-on:submit.prevent="doFillupEdit(fillup)")
+            .flex.flex-wrap.-m-3
+
+              label.m-3
+                p.text-gray-700.mb-2 Car
+                select(v-model="fillup.car" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-40")
+                    option(value='' disabled) -- Select a Car --
+                    option(v-for="car in uniqueCars" :value="car.car") {{car.car}}
+                button.new-car.block.mt-1(@click="addNewCar" class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-1 px-2 rounded-full text-xs mt-2") add car
+
+              label.m-3
+                p.text-gray-700.mb-2 Date
+
+                flat-pickr(v-model="fillup.date" :config="flatpickr_config" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-56")
+              label.m-3
+                p.text-gray-700.mb-2 Starting Odometer
+                input(type="text" v-model="fillup.starting_odo" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-40") 
+              label.m-3
+                p.text-gray-700.mb-2 Ending Odometer            
+                input(type="text" v-model="fillup.ending_odo" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-40") 
+              label.m-3
+                p.text-gray-700.mb-2 Miles
+                div.leading-tight.py-2(v-if="fillup.ending_odo && fillup.starting_odo") {{fillup.ending_odo - fillup.starting_odo}}
+
+              label.m-3
+                p.text-gray-700.mb-2 Starting Odometer              
+                input(type="text" v-model="fillup.gallons" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-40 mr-4") 
+
+     
+
+
+                button.save(:disabled="!isValid(fillup)" :class="{'opacity-50 pointer-events-none': !isValid(fillup), 'hover:bg-orange-700': isValid(fillup) }" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-1 px-3 rounded-full text-xs") Save
+
+                button.cancel(@click="cancelFillupEdit(fillup)" class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-1 px-3 rounded-full text-xs ml-2") Cancel
+            
+          div(v-else)
+            ul.mb-5.pt-5
+              .flex
+                div
+                  li 
+                    div.rounded.text-bg.px-2.py-1.leading-tight.mr-2.border-solid.border-red-500.border-2
+                      div MPG
+                      span.text-2xl.font-bold {{fillup_avgMPG(fillup.ending_odo - fillup.starting_odo, fillup.gallons)}}            
+                div.leading-relaxed
+                  li {{dateString(fillup.date)}} 
+                  div.flex.-m-1
+                    li.m-1
+                      span.font-bold Car: 
+                      | {{fillup.car}} 
+                    li.m-1
+                      span.font-bold Miles: 
+                      | {{fillup.ending_odo - fillup.starting_odo}} 
+                    li.m-1
+                      span.font-bold Gallons: 
+                      | {{fillup.gallons}} 
+                  li.mt-1
+                    button.destroy(@click="removeFillup(fillup)" class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-1 px-3 rounded-full text-xs") Remove
+                    button.edit(@click="editFillup(fillup)" class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-1 px-3 rounded-full text-xs ml-2") Edit
+
+
+    .footer.py-5.px-10.bg-fg
+      .max-w-6xl.mx-auto
+        .m.flex.items-center(class="lg:px-8")
+          span.letter M
+          p 
+            a(href="https://kpmcguire.github.io") McGuire Gas Company
+
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script src="./app.js"></script>
+<style lang="scss" src="./app.scss"></style>
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/app.js
@@ -0,0 +1,221 @@
+var STORAGE_KEY = 'todos-vuejs';
+var FILLUPSTORAGE_KEY = 'gascan-fillups'
+import _ from 'lodash'
+import flatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
+
+
+var fillupStorage = {
+  fetch: function(){
+    return JSON.parse(localStorage.getItem(FILLUPSTORAGE_KEY) || '[]')
+  },
+  save: function (fillups){
+    localStorage.setItem(FILLUPSTORAGE_KEY, JSON.stringify(fillups))
+  }
+}
+
+var todoStorage = {
+  fetch: function () {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      fillups: fillupStorage.fetch(),
+      todos: todoStorage.fetch(),
+      fillup_gallons: '',
+      fillup_car: '',
+      fillup_date: new Date(),
+      flatpickr_config: {
+        altInput: true,
+        altFormat: 'M j, Y h:i K',
+        enableTime: true
+      },
+      fillup_starting_odo: '',
+      fillup_ending_odo: '',
+      new_car: '',
+      show_new_fillup: false,
+      cache: {},
+      record: {}
+    }
+  },
+  watch: {
+    fillups: {
+      deep: true,
+      handler: fillupStorage.save
+    }
+  },
+  components: {
+    flatPickr
+  },
+  computed: {
+    total_avgMPG: function() {
+
+      let cars = {}
+      this.fillups.forEach(function (item) {
+        var list = cars[item.car]
+
+        if (list) {
+          list.push(item)
+        } else {
+          cars[item.car] = [item]
+        }
+      })
+
+      let output = {}
+
+      Object.keys(cars).forEach(function (key) {
+        let name = ''
+        let gallons = 0
+        let miles = 0
+
+        cars[key].forEach(function (car) {
+          name = car.car
+          let miles_driven = Number(car.ending_odo - car.starting_odo)
+          let gallons_filled = Number(car.gallons)
+          miles += miles_driven
+          gallons += gallons_filled
+        })
+
+        output[name] = (miles/gallons).toFixed(3)
+
+      })
+
+      return(output)
+    },
+    uniqueCars() {
+      return this.fillups.reduce((seed, current) => {
+        return Object.assign(seed, {
+          [current.car]: current
+        });
+      }, {});      
+    }, 
+    recentFillups() {
+      return _.sortBy(this.fillups, 'date').reverse()
+    },
+  },
+  methods: {
+
+    fillup_avgMPG: function(miles, gallons) {
+      return (miles/gallons).toFixed(1)
+    },
+
+    pluralize: function (word, count) {
+      return word + (count === 1 ? '' : 's');
+    },
+
+    addFillup: function() {
+      let car = this.fillup_car
+      let date = this.fillup_date
+      let gallons = this.fillup_gallons
+      let ending_odo = this.fillup_ending_odo
+      let starting_odo = this.fillup_starting_odo
+
+      this.fillups.push({
+        id: this.fillups.length + 1,
+        car: car,
+        date: date,
+        gallons: gallons,
+        ending_odo: ending_odo,
+        starting_odo: starting_odo,
+        editing: false
+      })
+      this.fillup_gallons = ''
+      this.fillup_date = new Date()
+      this.fillup_ending_odo = ''
+      this.fillup_starting_odo = this.recentFillups[0].ending_odo
+      this.show_new_fillup = false
+    },
+    addNewCarEdit: function(item) {
+      this.new_car = window.prompt('Nickname for Car:')
+      item.car = this.new_car
+    },
+    addNewCar: function() {
+      this.new_car = window.prompt('Nickname for Car:')
+      this.fillup_car = this.new_car
+    },
+    isValid: function(fillup) {
+      if (
+        fillup.car != '' && 
+        fillup.gallons != '' && 
+        fillup.ending_odo != '' && 
+        fillup.starting_odo != '' && 
+        fillup.date != '') {
+        return true
+      } else {
+        return false
+      }
+    },
+    isValidNew: function() {
+      if (
+        this.fillup_car != '' && 
+        this.fillup_gallons != '' && 
+        this.fillup_ending_odo != '' && 
+        this.fillup_starting_odo != '' && 
+        this.fillup_date != '') {
+        return true
+      } else {
+        return false
+      }
+    },    
+
+    removeFillup: function(fillup) {
+      let index = this.fillups.indexOf(fillup)
+      this.fillups.splice(index, 1)
+    },
+
+    removeTodo: function (todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+
+    editTodo: function (todo) {
+      this.beforeEditCache = todo.title;
+      this.editedTodo = todo;
+    },
+
+    editFillup: function(record) {
+      record.editing = true
+      this.record = _.cloneDeep(record);
+      this.cache = record;
+    },
+
+    cancelFillupEdit: function(record) {
+      Object.assign(record, this.record)
+      record.editing = false
+      this.cache = {}
+      this.record = {}
+    },
+
+    doFillupEdit: function (record) {
+      record.editing = false
+      var index = _.indexOf(this.fillups, this.cache);
+      this.fillups.splice(index, 1, record);
+    },
+    dateString: function(date) {
+      let options = {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        weekday: 'short',
+        hour: 'numeric',
+        minute: 'numeric'
+      }
+      return new Date(date).toLocaleString('en-US', options)
+    }
+  },
+  created() {
+    if (this.recentFillups[0].car) {
+      this.fillup_car = this.recentFillups[0].car
+    }
+
+    this.fillup_starting_odo = this.recentFillups[0].ending_odo
+  }
+
+}
