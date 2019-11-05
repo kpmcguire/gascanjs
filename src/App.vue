26e21fd4151516f9357248a8f16738204c57cf9c
Kevin McGuire <kevin@givegab.com>
2019-11-04 19:15:50 -0500

    allow decimal points, fix some spacing

--- src/App.vue
+++ src/App.vue
@@ -22,26 +22,26 @@
               label(for="new_car")
                 span.text-gray-700 Car                 
               button.new-car(@click="addNewCar" class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-1 px-3 rounded-full text-xs ml-2") add car
-              select#new_car(v-model="fillup_car" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline block w-full mt-2")
+              select#new_car(v-model="fillup_car" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-4 rounded leading-tight focus:outline-none focus:shadow-outline block w-full mt-2")
                 option(value='' disabled) -- Select a Car --
                 option(v-if="new_car" :value="fillup_car") {{fillup_car}}
                 option(v-for="car in uniqueCars" :value="car.car") {{car.car}}
 
             label.m-3
               p.text-gray-700.mb-2 Date
-              flat-pickr(v-model="fillup_date" :config="flatpickr_config" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-56")
+              flat-pickr(v-model="fillup_date" :config="flatpickr_config" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-4 rounded leading-tight focus:outline-none focus:shadow-outline w-56")
             label.m-3
               p.text-gray-700.mb-2 Starting Odometer
-              input.fillup-starting-odo(type="text" v-model="fillup_starting_odo" pattern="[0-9]*" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline")
+              input.fillup-starting-odo(type="number" v-model="fillup_starting_odo" pattern="\d*" step="0.001" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded leading-tight focus:outline-none focus:shadow-outline")
             label.m-3
               p.text-gray-700.mb-2 Ending Odometer
-              input.fillup-ending-odo(type="text" v-model="fillup_ending_odo" pattern="[0-9]*" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline") 
+              input.fillup-ending-odo(type="number" v-model="fillup_ending_odo" pattern="\d*" step="0.001" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded leading-tight focus:outline-none focus:shadow-outline") 
             label.m-3
               p.text-gray-700.mb-2 Miles      
               div.leading-tight.py-2(v-if="fillup_ending_odo && fillup_starting_odo") {{(fillup_ending_odo - fillup_starting_odo).toFixed(2)}}
             label.m-3
               p.text-gray-700.mb-2 Gallons
-              input.fillup-gallons(type="text" v-model="fillup_gallons" pattern="[0-9]*" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline")
+              input.fillup-gallons(type="number" v-model="fillup_gallons" pattern="\d*" step="0.001" class="w-40 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded leading-tight focus:outline-none focus:shadow-outline")
           div
             button.fillup-submit.block.mt-5(type="button" :disabled="!isValidNew()" @click="addFillup()" :class="{'opacity-50 pointer-events-none': !isValidNew(), 'hover:bg-orange-700': isValidNew() }" class="inline-block bg-orange-500 text-white font-bold py-2 px-4 rounded-full") Save
             button(@click="show_new_fillup = !show_new_fillup" class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full ml-2") Cancel
@@ -54,27 +54,27 @@
                 label(for="new_car")
                   span.text-gray-700 Car                 
                 button.new-car(@click="addNewCarEdit" class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-1 px-3 rounded-full text-xs ml-2") add car
-                select#new_car(v-model="fillup.car" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline block w-full mt-2")
+                select#new_car(v-model="fillup.car" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-4 rounded leading-tight focus:outline-none focus:shadow-outline block w-full mt-2")
                   option(value='' disabled) -- Select a Car --
                   option(v-for="car in uniqueCars" :value="car.car") {{car.car}}
 
               label.m-3
                 p.text-gray-700.mb-2 Date
 
-                flat-pickr(v-model="fillup.date" :config="flatpickr_config" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-56")
+                flat-pickr(v-model="fillup.date" :config="flatpickr_config" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-4 rounded leading-tight focus:outline-none focus:shadow-outline w-56")
               label.m-3
                 p.text-gray-700.mb-2 Starting Odometer
-                input(type="text" v-model="fillup.starting_odo" pattern="[0-9]*" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-40") 
+                input(type="number" v-model="fillup.starting_odo" pattern="\d*" step="0.001" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded leading-tight focus:outline-none focus:shadow-outline w-40") 
               label.m-3
                 p.text-gray-700.mb-2 Ending Odometer            
-                input(type="text" v-model="fillup.ending_odo" pattern="[0-9]*" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-40") 
+                input(type="number" v-model="fillup.ending_odo" pattern="\d*" step="0.001" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded leading-tight focus:outline-none focus:shadow-outline w-40") 
               label.m-3
                 p.text-gray-700.mb-2 Miles
                 div.leading-tight.py-2(v-if="fillup.ending_odo && fillup.starting_odo") {{(fillup.ending_odo - fillup.starting_odo).toFixed(2)}}
 
               label.m-3
-                p.text-gray-700.mb-2 Starting Odometer              
-                input(type="text" v-model="fillup.gallons" pattern="[0-9]*" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline w-40 mr-4") 
+                p.text-gray-700.mb-2 Gallons
+                input(type="number" v-model="fillup.gallons" pattern="\d*" step="0.001" class="bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded leading-tight focus:outline-none focus:shadow-outline w-40 mr-4") 
 
               .m-3.self-end
                 button.save(:disabled="!isValid(fillup)" :class="{'opacity-50 pointer-events-none': !isValid(fillup), 'hover:bg-orange-700': isValid(fillup) }" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-1 px-3 rounded-full text-xs") Save
